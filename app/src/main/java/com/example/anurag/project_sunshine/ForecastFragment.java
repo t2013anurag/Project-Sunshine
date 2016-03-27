cf1222aeb8dc6462ced414f8b525e387cedbf241
t2013anurag <tiwari.anurag126@gmail.com>
2016-03-27 15:25:03 +0530

    Added Network Requests

---
+++ app/src/main/java/com/example/anurag/project_sunshine/ForecastFragment.java
@@ -0,0 +1,124 @@
+package com.example.anurag.project_sunshine;
+
+/**
+ * Created by anurag on 27/3/16.
+ */
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+
+    ArrayAdapter<String> mForecastAdapter;
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/40",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Asteroids - 75/65",
+                "Fri - Heavy Rain - 65/56",
+                "Sat - Help trapped in weather station - 60/51",
+                "Sun - Sunny - 80/68"
+        };
+        List<String> weekForecast = new ArrayList<String>(
+                Arrays.asList(forecastArray));
+
+        mForecastAdapter = new ArrayAdapter<String>(
+                // Four params are passed first to get the application context
+                getActivity(),
+                // to get the layout of list
+                R.layout.list_item_forecast,
+                // to get the id of the textview
+                R.id.list_item_forecast_textview,
+                //finally the data to populate
+                forecastArray);
+
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            //HTTP request to API
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            //stores JSON
+            String forecastJsonstr = null;
+
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=17&appid=9b9254b072ef38ed9866940a98200973");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                forecastJsonstr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e("Placeholder Fragment", "Error", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("Placeholder Fragment", "Error closing stream", e);
+                    }
+                }
+            }
+            return  null;
+        }
+    }
+}
--- app/src/main/java/com/example/anurag/project_sunshine/mainActivity.java
+++ app/src/main/java/com/example/anurag/project_sunshine/mainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,7 +16,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import java.lang.StringBuffer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,7 +36,7 @@ public class mainActivity extends AppCompatActivity {
         setContentView(R.layout.layout);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -56,48 +63,6 @@ public class mainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        ArrayAdapter<String> mForecastAdapter;
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            String[] forecastArray = {
-                    "Today - Sunny - 88/63",
-                    "Tomorrow - Foggy - 70/40",
-                    "Weds - Cloudy - 72/63",
-                    "Thurs - Asteroids - 75/65",
-                    "Fri - Heavy Rain - 65/56",
-                    "Sat - Help trapped in weather station - 60/51",
-                    "Sun - Sunny - 80/68"
-            };
-            List<String> weekForecast = new ArrayList<String>(
-                    Arrays.asList(forecastArray));
-
-            mForecastAdapter = new ArrayAdapter<String>(
-                    // Four params are passed first to get the application context
-                    getActivity(),
-                    // to get the layout of list
-                    R.layout.list_item_forecast,
-                    // to get the id of the textview
-                    R.id.list_item_forecast_textview,
-                    //finally the data to populate
-                    forecastArray);
-
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(mForecastAdapter);
-            return rootView;
-        }
-    }
 }
 
