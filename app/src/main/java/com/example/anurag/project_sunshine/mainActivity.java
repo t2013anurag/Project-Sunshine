c738a2c8ca56c52d2f932c1ed2ec680ba41bf26c
t2013anurag <tiwari.anurag126@gmail.com>
2016-03-30 18:48:45 +0530

    View on Maps the location

--- app/src/main/java/com/example/anurag/project_sunshine/mainActivity.java
+++ app/src/main/java/com/example/anurag/project_sunshine/mainActivity.java
@@ -5,7 +5,10 @@ package com.example.anurag.project_sunshine;
  */
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -30,6 +33,8 @@ import java.util.List;
 
 
 public class mainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = mainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,10 +66,32 @@ public class mainActivity extends AppCompatActivity {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
+        if(id == R.id.action_map) {
+            openPreferredLocationOnMap();
+            return  true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void openPreferredLocationOnMap(){
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
+    }
 
 }
 
