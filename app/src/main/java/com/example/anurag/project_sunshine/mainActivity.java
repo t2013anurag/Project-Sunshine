0b793595c55799234bb897505fb346432d03a7ad
t2013anurag <tiwari.anurag126@gmail.com>
2016-04-13 16:53:47 +0530

    Database

--- app/src/main/java/com/example/anurag/project_sunshine/mainActivity.java
+++ app/src/main/java/com/example/anurag/project_sunshine/mainActivity.java
@@ -3,7 +3,6 @@ package com.example.anurag.project_sunshine;
 /**
  * Created by anurag on 26/3/16.
  */
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
