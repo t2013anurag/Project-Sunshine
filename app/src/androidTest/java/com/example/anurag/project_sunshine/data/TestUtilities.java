f32101f5660a5f7771bdf3304be8fda179c6d94d
t2013anurag <tiwari.anurag126@gmail.com>
2016-04-13 18:02:07 +0530

    Classes and Packages renamed

--- app/src/androidTest/java/com/example/anurag/project_sunshine/data/TestUtilities.java
+++ app/src/androidTest/java/com/example/anurag/project_sunshine/data/TestUtilities.java
@@ -1,4 +1,4 @@
-package com.example.android.sunshine.app.data;
+package com.example.anurag.project_sunshine.data;
 
 import android.content.ContentValues;
 import android.database.ContentObserver;
@@ -8,7 +8,7 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.test.AndroidTestCase;
 
-import com.example.android.sunshine.app.utils.PollingCheck;
+import com.example.anurag.project_sunshine.utils.PollingCheck;
 
 import java.util.Map;
 import java.util.Set;
--- app/src/androidTest/java/com/example/anurag/project_sunshine/utils/PollingCheck.java
+++ app/src/androidTest/java/com/example/anurag/project_sunshine/utils/PollingCheck.java
@@ -15,7 +15,7 @@
  *
  * Note: This file copied from the Android CTS Tests
  */
-package com.example.android.sunshine.app.utils;
+package com.example.anurag.project_sunshine.utils;
 
 import junit.framework.Assert;
 
